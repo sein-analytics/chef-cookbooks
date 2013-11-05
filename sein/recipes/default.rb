183f37e0f219381a3807d680d1365cb30ec8b19f
mkherlakian <mk@vivaxis.ca>
2013-11-05 17:20:20 -0500

    added requirements for Gearman and igbinary

--- sein/recipes/default.rb
+++ sein/recipes/default.rb
@@ -7,8 +7,28 @@
 # All rights reserved - Do Not Redistribute
 #
 
+package "automake" do
+  action :install
+end
+
 #install igbinary from PECL
 zendserver_pear "igbinary" do
+  action :install
+end
+
+apt_repository "gearman" do
+  uri "http://ppa.launchpad.net/gearman-developers/ppa/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "1C73E014"
+end
+
+package "libgearman-dev" do
+  action :install
+end
+
+package "gearman"
   action :install
 end
 
