42dda070632ecb5ed84fad01e7c5439c2acc54a6
mkherlakian <mk@vivaxis.ca>
2014-06-09 14:09:03 -0400

    moved xdebug and xhprof out of sein::zendserver recipe

--- sein/recipes/zendserver.rb
+++ sein/recipes/zendserver.rb
@@ -1,26 +1,5 @@
 include_recipe "zendserver::single"
 
-# install xhprof extension
-zendserver_pear "xhprof" do
-  action :install
-  preferred_state "beta"
-  notifies :restart, 'service[zend-server]'
-end
-
-zendserver_pear "xdebug" do
-  action :install
-  notifies :restart, 'service[zend-server]'
-end
-
-file "/usr/local/zend/etc/conf.d/xdebug.ini" do
-  action :delete
-end
-
-template "/usr/local/zend/etc/conf.d/zend_extension_manager.ini" do
-  source "xdebug_zem.ini.erb"
-  notifies :run, 'execute[restart-api]'
-end
-
 zendserver_pear "mongo" do
   action :install
   notifies :restart, 'service[zend-server]'
