f7c9a48902801efb6363f204ee052f9525317f2a
mkherlakian <mk@vivaxis.ca>
2014-06-12 19:25:44 -0400

    changed build vars to be passed dynamically

--- sein/attributes/default.rb
+++ sein/attributes/default.rb
@@ -1,50 +1,51 @@
-default[:sein][:project_root]					= '/vagrant_data'
 default[:sein][:server_name]					= 'sein.vagrant'
 default[:sein][:server_aliases]					= ['sein']
 
-default[:sein][:display_startup_errors] 		= 0
-default[:sein][:display_errors] 				= 0
-default[:sein][:display_exceptions] 			= 0
+default[:sein][:phing][:project_root]					= '/vagrant_data'
+
+default[:sein][:phing][:display_startup_errors] 		= 0
+default[:sein][:phing][:display_errors] 				= 0
+default[:sein][:phing][:display_exceptions] 			= 0
 
 # SMTP settings
-default[:sein][:smtp_host] 						= "smtp.gmail.com"
-default[:sein][:smtp_auth] 						= "login"
-default[:sein][:smtp_username] 					= "samuel.okunde@gmail.com"
-default[:sein][:smtp_password] 					= "Xd8O$F:/22|)_T"
-default[:sein][:smtp_ssl] 						= "ssl"
-default[:sein][:smtp_port] 						= "465"
+default[:sein][:phing][:smtp_host] 						= "smtp.gmail.com"
+default[:sein][:phing][:smtp_auth] 						= "login"
+default[:sein][:phing][:smtp_username] 					= "samuel.okunde@gmail.com"
+default[:sein][:phing][:smtp_password] 					= "Xd8O$F:/22|)_T"
+default[:sein][:phing][:smtp_ssl] 						= "ssl"
+default[:sein][:phing][:smtp_port] 						= "465"
 
 # Default database settings (ZF data table)
-default[:sein][:default_db_adapter] 			= 'PDO_MYSQL'
-default[:sein][:default_db_host] 				= ''
-default[:sein][:default_db_username] 			= ''
-default[:sein][:default_db_password] 			= ''
-default[:sein][:default_db_name] 				= ''
-default[:sein][:default_db_socket] 				= ''
+default[:sein][:phing][:default_db_adapter] 			= 'PDO_MYSQL'
+default[:sein][:phing][:default_db_host] 				= ''
+default[:sein][:phing][:default_db_username] 			= ''
+default[:sein][:phing][:default_db_password] 			= ''
+default[:sein][:phing][:default_db_name] 				= ''
+default[:sein][:phing][:default_db_socket] 				= ''
 
 # Deals database
-default[:sein][:deal_db_driver] 				= 'pdo_mysql'
-default[:sein][:deal_db_host] 					= ''
-default[:sein][:deal_db_port] 					= ''
-default[:sein][:deal_db_user] 					= ''
-default[:sein][:deal_db_password] 				= ''
+default[:sein][:phing][:deal_db_driver] 				= 'pdo_mysql'
+default[:sein][:phing][:deal_db_host] 					= ''
+default[:sein][:phing][:deal_db_port] 					= ''
+default[:sein][:phing][:deal_db_user] 					= ''
+default[:sein][:phing][:deal_db_password] 				= ''
 
 # Doctrine
-default[:sein][:doctrine_db_driver] 			= 'pdo_mysql'
-default[:sein][:doctrine_db_name] 				= ''
-default[:sein][:doctrine_db_host] 				= ''
-default[:sein][:doctrine_db_port] 				= ''
-default[:sein][:doctrine_db_user] 				= ''
-default[:sein][:doctrine_db_password] 			= ''
-default[:sein][:doctrine_db_socket] 			= ''
+default[:sein][:phing][:doctrine_db_driver] 			= 'pdo_mysql'
+default[:sein][:phing][:doctrine_db_name] 				= ''
+default[:sein][:phing][:doctrine_db_host] 				= ''
+default[:sein][:phing][:doctrine_db_port] 				= ''
+default[:sein][:phing][:doctrine_db_user] 				= ''
+default[:sein][:phing][:doctrine_db_password] 			= ''
+default[:sein][:phing][:doctrine_db_socket] 			= ''
 
-default[:sein][:mongodb_host] 					= ''
-default[:sein][:mongodb_port] 					= ''
-default[:sein][:mongodb_db] 					= ''
-default[:sein][:mongodb_user] 					= ''
-default[:sein][:mongodb_pass] 					= ''
+default[:sein][:phing][:mongodb_host] 					= ''
+default[:sein][:phing][:mongodb_port] 					= ''
+default[:sein][:phing][:mongodb_db] 					= ''
+default[:sein][:phing][:mongodb_user] 					= ''
+default[:sein][:phing][:mongodb_pass] 					= ''
 
-default[:sein][:gearman_servers]				= '127.0.0.1:4730'
+default[:sein][:phing][:gearman_servers]				= '127.0.0.1:4730'
 
-default[:sein][:doctrine_cache_adapter_class]	= 'Doctrine\Common\Cache\ArrayCache'
-default[:sein][:doctrine_autogenerate_classes]  = 'true'
+default[:sein][:phing][:doctrine_cache_adapter_class]	= 'Doctrine\Common\Cache\ArrayCache'
+default[:sein][:phing][:doctrine_autogenerate_classes]  = 'true'
--- sein/recipes/configs.rb
+++ sein/recipes/configs.rb
@@ -13,51 +13,26 @@ directory COMPOSER_HOME do
 	action :create
 end
 
+# ruby_block "build phing" do
+# 	block do
+	command_phing = lambda {
+		command = "/usr/local/zend/bin/phing builddev "
+		node[:sein][:phing].each do |name, prop|
+		    command += "-D#{name}=\"#{prop}\" " 
+ 		end
+
+ 		return command
+ 	}
+
+# 	end
+# end
+
 # Not using templates to keep process centralized - using Phing for replacements so that CI server can also call task
 execute "phing tasks" do
-  command %Q{
-  	/usr/local/zend/bin/phing builddev \
--Dphp.path="/usr/local/zend/bin/" \
--Dapplication.display_startup_errors="#{node[:sein][:display_startup_errors]}" \
--Dapplication.display_errors="#{node[:sein][:display_errors]}" \
--Dapplication.display_exceptions="#{node[:sein][:display_exceptions]}" \
--Dapplication.smtp_host="#{node[:sein][:smtp_host]}" \
--Dapplication.smtp_auth="#{node[:sein][:smtp_auth]}" \
--Dapplication.smtp_username="#{node[:sein][:smtp_username]}" \
--Dapplication.smtp_password="#{node[:sein][:smtp_password]}" \
--Dapplication.smtp_ssl="#{node[:sein][:smtp_ssl]}" \
--Dapplication.smtp_port="#{node[:sein][:smtp_port]}" \
--Dapplication.default_db_adapter="#{node[:sein][:default_db_adapter]}" \
--Dapplication.default_db_host="#{node[:sein][:default_db_host]}" \
--Dapplication.default_db_username="#{node[:sein][:default_db_username]}" \
--Dapplication.default_db_password="#{node[:sein][:default_db_password]}" \
--Dapplication.default_db_name="#{node[:sein][:default_db_name]}" \
--Dapplication.default_db_socket="#{node[:sein][:default_db_socket]}" \
--Dapplication.deal_db_driver="#{node[:sein][:deal_db_driver]}" \
--Dapplication.deal_db_host="#{node[:sein][:deal_db_host]}" \
--Dapplication.deal_db_port="#{node[:sein][:deal_db_port]}" \
--Dapplication.default_db_socket="#{node[:sein][:default_db_socket]}" \
--Dapplication.deal_db_user="#{node[:sein][:deal_db_user]}" \
--Dapplication.deal_db_password="#{node[:sein][:deal_db_password]}" \
--Dapplication.doctrine_db_driver="#{node[:sein][:doctrine_db_driver]}" \
--Dapplication.doctrine_db_name="#{node[:sein][:doctrine_db_name]}" \
--Dapplication.doctrine_db_host="#{node[:sein][:doctrine_db_host]}" \
--Dapplication.doctrine_db_port="#{node[:sein][:doctrine_db_port]}" \
--Dapplication.doctrine_db_user="#{node[:sein][:doctrine_db_user]}" \
--Dapplication.doctrine_db_password="#{node[:sein][:doctrine_db_password]}" \
--Dapplication.doctrine_db_socket="#{node[:sein][:doctrine_db_socket]}" \
--Dapplication.doctrine_cache_adapter_class="#{node[:sein][:doctrine_cache_adapter_class]}" \
--Dapplication.doctrine_autogenerate_classes="#{node[:sein][:doctrine_autogenerate_classes]}" \
--Dapplication.mongodb_host="#{node[:sein][:mongodb_host]}" \
--Dapplication.mongodb_port="#{node[:sein][:mongodb_port]}" \
--Dapplication.mongodb_db="#{node[:sein][:mongodb_db]}" \
--Dapplication.mongodb_user="#{node[:sein][:mongodb_user]}" \
--Dapplication.mongodb_pass="#{node[:sein][:mongodb_pass]}" \
--Dapplication.mongodb_pass="#{node[:sein][:gearman_servers]}" 
-}
+  command command_phing.call
   user "vagrant"
   group "vagrant"
-  cwd node[:sein][:project_root]
+  cwd node[:sein][:phing][:project_root]
   environment ({"COMPOSER_HOME" => COMPOSER_HOME})
 end
 
