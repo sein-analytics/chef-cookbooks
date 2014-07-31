20f845d66b3af90ec22420ad8af1a11c9bb90dc5
mkherlakian <mk@vivaxis.ca>
2014-07-31 14:01:53 -0400

    added synapse config

--- sein/attributes/synapse.rb
+++ sein/attributes/synapse.rb
@@ -1,22 +1,20 @@
 default.smartstack.services.mysql = {
-  'mysql' => {
-    'synapse' => {
-      'discovery' => { 
-      	'method' => 'ec2tag',
-      	'tag_name' => 'opsworks:layer:mysql',
-      	'tag_value' => 'MySQL',
-      	'aws_access_key_id' => 'AKIAJVFAK5A4QL5WABIA',
-      	'aws_secret_access_key' => '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',
-      	'aws_region' => 'us-east-1'
-      },
-      'haproxy' => {
-        'server_options' => 'check inter 1s rise 1 fall 1',
-        'listen' => [
-          'mode tcp'
-        ]
-      },
-    }
-  }
+	'synapse' => {
+	  'discovery' => { 
+	  	'method' => 'ec2tag',
+	  	'tag_name' => 'opsworks:layer:mysql',
+	  	'tag_value' => 'MySQL',
+	  	'aws_access_key_id' => 'AKIAJVFAK5A4QL5WABIA',
+	  	'aws_secret_access_key' => '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',
+	  	'aws_region' => 'us-east-1'
+	  },
+	  'haproxy' => {
+	    'server_options' => 'check inter 1s rise 1 fall 1',
+	    'listen' => [
+	      'mode tcp'
+	    ]
+	  },
+	}
 }
 
 default.synapse.enabled_services = ['mysql']
