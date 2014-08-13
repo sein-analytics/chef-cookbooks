ffb15a61adab0b56f17f75252acbeb3613c0224f
mkherlakian <mk@vivaxis.ca>
2014-08-13 16:16:47 -0400

    fixed syntax sudoers

--- sein/recipes/sudoers.rb
+++ sein/recipes/sudoers.rb
@@ -3,5 +3,6 @@ include_recipe "sudo"
 sudo "seinworkers" do
 	user "zend"
 	runas "root"
-	commands ["supervisorctl restart seinworkers:"]
+	nopasswd true
+	commands ["/usr/bin/supervisorctl restart seinworkers\\:"]
 end
